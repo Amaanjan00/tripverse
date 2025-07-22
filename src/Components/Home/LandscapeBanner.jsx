9f6cbd751b414253b9d53192a68b453d00580680
Amaanjan00 <amaan.jan@bba.christuniversity.in>
2025-07-22 21:44:48 +0400

    transition added

--- src/Components/Home/LandscapeBanner.jsx
+++ src/Components/Home/LandscapeBanner.jsx
@@ -8,8 +8,8 @@ function LandscapeBanner() {
     <>
       <div className='p-5 lg:p-10 w-full h-fit'>
         <div className='flex flex-col lg:flex-row w-full justify-between gap-5 lg:gap-15 rounded-2xl'>
-          <div className='w-full'><Link to="/umrahpackage"><img className='rounded-2xl shadow-2xl' src={umrah} alt="" /></Link></div>
-          <div className='w-full'><Link to="/dubaipackage"><img className='rounded-2xl shadow-2xl' src={dubai} alt="" /></Link></div>
+          <div className='w-full hover:scale-102 transition-all duration-600'><Link to="/umrahpackage"><img className='rounded-2xl shadow-2xl' src={umrah} alt="" /></Link></div>
+          <div className='w-full hover:scale-102 transition-all duration-600'><Link to="/dubaipackage"><img className='rounded-2xl shadow-2xl' src={dubai} alt="" /></Link></div>
         </div>
       </div>
     </>
