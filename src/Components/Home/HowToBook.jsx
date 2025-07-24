3ce9adb9349e1ab2c331e06b283e0bd52fba92ac
Amaanjan00 <amaan.jan@bba.christuniversity.in>
2025-07-24 21:27:38 +0400

    how to book updated

--- src/Components/Home/HowToBook.jsx
+++ src/Components/Home/HowToBook.jsx
@@ -3,6 +3,7 @@ import search from '../Home/assets/1.svg'
 import sendinquiry from '../Home/assets/2.svg'
 import choosemethod from '../Home/assets/3.svg'
 import callback from '../Home/assets/4.svg'
+import enterdetails from '../Home/assets/enterdetails.png'
 
 function HowToBook() {
   return (
@@ -15,7 +16,7 @@ function HowToBook() {
 
         <div className='lg:px-10 bg-amber-600 lg:rounded-tl-[7rem] lg:rounded-br-[7rem] lg:rounded-[0] rounded-4xl'>
 
-          <div className='flex md:flex-row justify-evenly items-center flex-col gap-10 px-10 text-center'>
+          <div className='flex text-center md:flex-row justify-evenly items-center flex-col gap-10 px-10'>
 
             <div className='p-5 items-center justify-between uppercase text-white font-extrabold flex flex-col'>
               <img className='w-fit h-fit min-h-50 scale-55' src={search} alt="" />
@@ -29,12 +30,17 @@ function HowToBook() {
 
             <div className='p-5 items-center h-80 justify-between uppercase text-white font-extrabold flex flex-col'>
               <img className='w-fit h-fit min-h-50 scale-65' src={choosemethod} alt="" />
-              <h1 className='min-h-15'>Choose Email or WhatsApp, then send</h1>
+              <h1 className='min-h-15'>Select inquiry method - (Email/WhatsApp)</h1>
+            </div>
+
+            <div className='p-5 items-center h-80 justify-between uppercase text-white font-extrabold flex flex-col'>
+              <img className='w-fit h-fit min-h-50 scale-55' src={enterdetails} alt="" />
+              <h1 className='min-h-15'>Enter your required details and needs</h1>
             </div>
 
             <div className='p-5 items-center h-80 justify-between uppercase text-white font-extrabold flex flex-col'>
               <img className='w-fit h-fit min-h-50 scale-55' src={callback} alt="" />
-              <h1 className='min-h-15'>Within 2 Business Days <br /> we will contact you!</h1>
+              <h1 className='min-h-15'>Within 2 Business Days we will contact you!</h1>
             </div>
 
           </div>
