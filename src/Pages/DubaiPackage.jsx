d3c985f95d89c70ae296b4afefe815d5f81b2028
Amaanjan00 <amaan.jan@bba.christuniversity.in>
2025-07-28 18:32:38 +0400

    dubai packages page updated

--- src/Pages/DubaiPackage.jsx
+++ src/Pages/DubaiPackage.jsx
@@ -7,23 +7,194 @@ import dubai2 from '../assets/dubai2.png'
 import dubai3 from '../assets/dubai3.png'
 import Button from '../Components/Button'
 import ScrollableDubaiPackages from '../Components/ScrollableDubaiPackages'
+import camelridedubai from '../assets/dubaipackages/camelridedubai.jpg'
+import aindubairide from '../assets/dubaipackages/aindubairide.jpg'
+import dhowcruise from '../assets/dubaipackages/dhowcruise.jpg'
+import dubaifountain from '../assets/dubaipackages/dubaifountain.jpg'
+import dubaiframe from '../assets/dubaipackages/dubaiframe.jpg'
+import globalvillage from '../assets/dubaipackages/globalvillage.jpg'
+import goldsouk from '../assets/dubaipackages/goldsouk.jpg'
+import helicopterride from '../assets/dubaipackages/helicopterride.jpg'
+import hotairballon from '../assets/dubaipackages/hotairballon.jpg'
+import shoppingdubai from '../assets/dubaipackages/shoppingdubai.jpg'
+import skidubai from '../assets/dubaipackages/skidubai.jpg'
+import skydiving from '../assets/dubaipackages/skydiving.jpg'
+import yachtdubai from '../assets/dubaipackages/yachtdubai.jpg'
+import ziplinedubai from '../assets/dubaipackages/ziplinedubai.jpg'
+import dessertsafari from '../assets/dubaipackages/dessertsafari.jpg'
+import burjkhalifa from '../assets/dubaipackages/burjkhalifa.jpg'
+import dubaimall from '../assets/dubaipackages/dubaimall.jpg'
+import palmjumeirah from '../assets/dubaipackages/palmjumeirah.jpg'
+import dubaimarina from '../assets/dubaipackages/dubaimarina.jpg'
+import museumofthefuture from '../assets/dubaipackages/museumofthefuture.jpg'
+import atlantis from '../assets/dubaipackages/atlantis.jpg'
+import dubaiquarium from '../assets/dubaipackages/dubaiquarium.jpg'
+import miraclegarden from '../assets/dubaipackages/miraclegarden.jpg'
+import dubaicreek from '../assets/dubaipackages/dubaicreek.jpg'
+import jbrwalk from '../assets/dubaipackages/jbrwalk.jpg'
+import alfahididistrict from '../assets/dubaipackages/alfahididistrict.jpg'
+import bluewatersisland from '../assets/dubaipackages/bluewatersisland.jpg'
+import dubaiopera from '../assets/dubaipackages/dubaiopera.jpg'
+import skyviewdubai from '../assets/dubaipackages/skyviewdubai.jpg'
+import lamerbeach from '../assets/dubaipackages/lamerbeach.jpg'
 
 function DubaiPackage() {
 
-  const placestovisitindubai = [
-    {
-      nameofplace: "",
-      shortdescription: "",
-      image: "abc"
-    },
-  ]
-
   const thingstodoindubai = [
     {
-      nameofthing: "",
-      description: "",
-      img: "abc"
-    }
+      nameofplace: "Desert Safari",
+      shortdescription: "Thrilling dune bashing with sunset views",
+      image: dessertsafari
+    },
+    {
+      nameofplace: "Dhow Cruise",
+      shortdescription: "Dinner and dance on a floating boat",
+      image: dhowcruise
+    },
+    {
+      nameofplace: "Skydiving Dubai",
+      shortdescription: "Skydive over iconic Palm Jumeirah",
+      image: skydiving
+    },
+    {
+      nameofplace: "Dubai Frame",
+      shortdescription: "Cross the bridge of past and future",
+      image: dubaiframe
+    },
+    {
+      nameofplace: "Ain Dubai Ride",
+      shortdescription: "Ride the world’s tallest observation wheel",
+      image: aindubairide
+    },
+    {
+      nameofplace: "Dubai Fountain",
+      shortdescription: "Watch choreographed water and light shows",
+      image: dubaifountain
+    },
+    {
+      nameofplace: "Helicopter Tour",
+      shortdescription: "Aerial views of Dubai’s famous landmarks",
+      image: helicopterride
+    },
+    {
+      nameofplace: "Hot Air Balloon",
+      shortdescription: "Sunrise flight over desert landscapes",
+      image: hotairballon
+    },
+    {
+      nameofplace: "Luxury Yacht Tour",
+      shortdescription: "Cruise past Dubai’s glittering skyline",
+      image: yachtdubai
+    },
+    {
+      nameofplace: "Ski Dubai",
+      shortdescription: "Snow adventure inside a giant mall",
+      image: skidubai
+    },
+    {
+      nameofplace: "Camel Ride",
+      shortdescription: "Traditional desert ride with scenic backdrop",
+      image: camelridedubai
+    },
+    {
+      nameofplace: "Zipline Marina",
+      shortdescription: "Glide over the stunning Dubai Marina",
+      image: ziplinedubai
+    },
+    {
+      nameofplace: "Shopping Spree",
+      shortdescription: "Explore luxury malls and souks",
+      image: shoppingdubai
+    },
+    {
+      nameofplace: "Gold Souk Walk",
+      shortdescription: "Shop glittering gold at traditional market",
+      image: goldsouk
+    },
+    {
+      nameofplace: "Global Village",
+      shortdescription: "Explore global cultures in one place",
+      image: globalvillage
+    },
+
+  ]
+
+  const placestovisitindubai = [
+    {
+      nameofthing: "Burj Khalifa",
+      description: "World's tallest tower with observation deck",
+      img: burjkhalifa
+    },
+    {
+      nameofthing: "Dubai Mall",
+      description: "Iconic shopping and entertainment destination",
+      img: dubaimall
+    },
+    {
+      nameofthing: "Palm Jumeirah",
+      description: "Man-made island with luxury attractions",
+      img: palmjumeirah
+    },
+    {
+      nameofthing: "Dubai Marina",
+      description: "Waterfront with skyline and restaurants",
+      img: dubaimarina
+    },
+    {
+      nameofthing: "Museum of Future",
+      description: "Innovative exhibits on tomorrow’s world",
+      img: museumofthefuture
+    },
+    {
+      nameofthing: "Atlantis Hotel",
+      description: "Iconic hotel with waterpark access",
+      img: atlantis
+    },
+    {
+      nameofthing: "Dubai Aquarium",
+      description: "Underwater tunnel with marine life",
+      img: dubaiquarium
+    },
+    {
+      nameofthing: "Miracle Garden",
+      description: "Floral displays in imaginative shapes",
+      img: miraclegarden
+    },
+    {
+      nameofthing: "Dubai Creek",
+      description: "Historic waterway with abra rides",
+      img: dubaicreek
+    },
+    {
+      nameofthing: "JBR Walk",
+      description: "Beachside promenade with dining and shops",
+      img: jbrwalk
+    },
+    {
+      nameofthing: "Al Fahidi District",
+      description: "Old Dubai with traditional wind towers",
+      img: alfahididistrict
+    },
+    {
+      nameofthing: "Bluewaters Island",
+      description: "Home to Ain Dubai and restaurants",
+      img: bluewatersisland
+    },
+    {
+      nameofthing: "Dubai Opera",
+      description: "Cultural shows in downtown’s heart",
+      img: dubaiopera
+    },
+    {
+      nameofthing: "La Mer Beach",
+      description: "Beach, cafes, and vibrant street art",
+      img: lamerbeach
+    },
+    {
+      nameofthing: "Sky Views Dubai",
+      description: "Glass slide and panoramic city views",
+      img: skyviewdubai
+    },
   ]
 
   return (
@@ -55,10 +226,10 @@ function DubaiPackage() {
 
             {placestovisitindubai.map((p, index) => (
               <div key={index} className='relative flex shrink-0 ml-5'>
-                <img className='h-90 w-70 object-cover rounded-3xl' src={p.image} alt="" />
+                <img className='h-90 w-70 object-cover rounded-3xl' src={p.img} alt="" />
                 <div className='absolute inset-x-0 bottom-0 p-5 bg-black/50 rounded-b-3xl'>
-                    <h1 className='text-2xl font-extrabold text-white'>{p.nameofplace}</h1>
-                    <p className='text-[14px] text-white'>{p.shortdescription}</p>
+                    <h1 className='text-2xl font-extrabold text-white'>{p.nameofthing}</h1>
+                    <p className='text-[14px] text-white'>{p.description}</p>
                 </div>
               </div>
             ))}
@@ -81,10 +252,10 @@ function DubaiPackage() {
 
             {thingstodoindubai.map((t, index) => (
               <div key={index} className='relative flex shrink-0 ml-5'>
-                <img className='h-90 w-70 object-cover rounded-3xl' src={t.img} alt="" />
+                <img className='h-90 w-70 object-cover rounded-3xl' src={t.image} alt="" />
                 <div className='absolute inset-x-0 bottom-0 p-5 bg-black/50 rounded-b-3xl'>
-                    <h1 className='text-2xl font-extrabold text-white'>{t.nameofthing}</h1>
-                    <p className='text-[14px] text-white'>{t.description}</p>
+                    <h1 className='text-2xl font-extrabold text-white'>{t.nameofplace}</h1>
+                    <p className='text-[14px] text-white'>{t.shortdescription}</p>
                 </div>
               </div>
             ))}
